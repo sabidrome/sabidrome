d3d2c16cee5141ed165ede34a929c34d724ae4d7
Tomás Villegas <tomas.villegas.m@ug.uchile.cl>
2025-12-12 15:38:30 -0300

    feat: add to db metadata from book on path

---
+++ add_book_to_db.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"database/sql"
+
+
+    "rsc.io/quote"
+	"github.com/taylorskalyo/goreader/epub"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+
+type Book struct {
+	Name string
+	Author string
+}
+
+type Database struct {
+    Type string
+    Path string
+}
+
+func GetBookMetadataFromPath(path string) ( bookObject Book ) {
+
+    rc, err := epub.OpenReader(path)
+    if err != nil {
+        panic(err)
+    }
+    defer rc.Close()
+
+    book := rc.Rootfiles[0]
+
+    bookObject = Book {
+        Name: book.Title,
+        Author: book.Creator,
+    }
+
+    fmt.Println(bookObject.Name)
+    fmt.Println(bookObject.Author)
+
+    return bookObject
+
+}
+
+// func CreateDatabase() (db *sql.DB) {
+
+func CreateDatabase(d *Database) {
+
+    // If db doesn't exist, it will create it
+    db, err := sql.Open(d.Type, d.Path)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println("Connected to the SQLite database succesfully.")
+
+    var sqliteVersion string
+    err = db.QueryRow("select sqlite_version()").Scan(&sqliteVersion)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    defer db.Close()
+
+    fmt.Println(sqliteVersion)
+
+}
+
+func CreateDatabaseBookshelfTable(d *Database) {
+
+    query := `CREATE TABLE IF NOT EXISTS bookshelf (
+            bookid INTEGER PRIMARY KEY AUTOINCREMENT,
+            name   TEXT NOT NULL,
+            author TEXT NOT NULL
+
+            );`
+
+    db, err := sql.Open(d.Type, d.Path)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    result, err := db.Exec(query)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println(result)
+    fmt.Println("Table 'bookshelf' was create succesfully.")
+
+}
+
+func AddBookToDatabase(d *Database, b *Book) {
+
+    query := `INSERT INTO bookshelf (name, author) VALUES (?, ?);`
+
+    db, err := sql.Open(d.Type, d.Path)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    result, err := db.Exec(query, b.Name, b.Author)
+    if err != nil {
+        return
+    }
+
+    fmt.Printf("Book %s was create succesfully.\n", b.Name)
+
+    fmt.Println(result.LastInsertId())
+
+}
+
+func main() {
+
+    fmt.Println("Hello, World!")
+    fmt.Println(quote.Go())
+
+    databaseObject := Database {
+        Type: "sqlite3",
+        Path: "./sabidrome.db",
+    }
+
+    CreateDatabase(&databaseObject)
+    CreateDatabaseBookshelfTable(&databaseObject)
+    book_struct := GetBookMetadataFromPath(os.Args[1])
+    AddBookToDatabase(&databaseObject, &book_struct)
+
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module sabidrome
+
+go 1.25.5
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/taylorskalyo/goreader v1.0.1
+	rsc.io/quote v1.5.2
+)
+
+require (
+	golang.org/x/text v0.22.0 // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
