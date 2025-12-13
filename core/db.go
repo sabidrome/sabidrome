a368c94aecdd10f3802dd862d1852c3490004ae0
Tomás Villegas <tomas.villegas.m@ug.uchile.cl>
2025-12-13 17:33:37 -0300

    refactor: improve debug messages

--- core/db.go
+++ core/db.go
@@ -44,6 +44,8 @@ func InitDatabase(d *Database) {
 
 func AddBookToDatabase(d *Database, b *Book) {
 
+    fmt.Printf("[Debug] '%s' requested for addition to database.", b.Name)
+
     query := `INSERT INTO bookshelf (name, author, path) VALUES (?, ?, ?);`
 
     db, err := sql.Open(d.Type, d.Path)
@@ -57,24 +59,22 @@ func AddBookToDatabase(d *Database, b *Book) {
         return
     }
 
-    fmt.Printf(" -> Book '%s' added to database.\n", b.Name)
+    fmt.Printf("[Debug] '%s' added to database succesfully.\n", b.Name)
 
 }
 
 func AddBook(db *Database, path string) {
 
-	fmt.Println("[Debug] Begin add a new book")
 	book_struct := GetBookMetadataFromPath(path)
 	AddBookToDatabase(db, &book_struct)
-	fmt.Println("[Debug] End add a new book")
 
 }
 
 func FindBookByName(d *Database, name string) (*Book, error) {
 
-    query := `SELECT * FROM bookshelf WHERE name LIKE '%' || ? || '%'`
+    fmt.Printf("[Debug] Looking '%s' as book name in database.", name)
 
-    // fmt.Println(query)
+    query := `SELECT * FROM bookshelf WHERE name LIKE '%' || ? || '%'`
 
     db, err := sql.Open(d.Type, d.Path)
     if err != nil {
@@ -83,21 +83,23 @@ func FindBookByName(d *Database, name string) (*Book, error) {
     }
 
     row   := db.QueryRow(query, name)
-    // fmt.Println(row)
     b     := &Book{}
 
     var id int
     err = row.Scan(&id, &b.Name, &b.Author, &b.Path)
     if err != nil {
-        // fmt.Println(err)
         return nil, err
     }
+
+    fmt.Printf("[Debug] Found match for '%s' in %s\n", name, b.Path)
 
     return b, nil
 
 }
 
 func FindBookByAuthor(d *Database, author string) (*Book, error) {
+
+    fmt.Printf("[Debug] Looking '%s' as book author in database.", author)
 
     query := `SELECT * FROM bookshelf WHERE author LIKE '%' || ? || '%'`
 
@@ -120,34 +122,32 @@ func FindBookByAuthor(d *Database, author string) (*Book, error) {
         return nil, err
     }
 
+    fmt.Printf("[Debug] Found match for '%s' in %s\n", author, b.Path)
+
     return b, nil
 
 }
 
 func FindBook(db *Database, query string) {
 
-    fmt.Println("[Debug] Being search a book")
     var book *Book
     var err error
 
     book, err = FindBookByName(db, query)
     if err != nil {
-        fmt.Printf(" -> '%s' is not a book name in the db\n", query)
+        fmt.Println("[Debug] There are no books matching.")
     }
 
     book, err = FindBookByAuthor(db, query)
     if err != nil {
-        fmt.Printf(" -> '%s' is not a book author in the db\n", query)
+        fmt.Println("[Debug] There are no authors matching")
     }
 
     if book != nil {
-        fmt.Println(" -> Found a match in the db")
-        fmt.Printf("    -> Name: %s\n", book.Name)
-        fmt.Printf("    -> Author: %s\n", book.Author)
-        fmt.Printf("    -> Path: %s\n", book.Path)
-    } else {
-        fmt.Println(" -> Did not find a match in the db")
+        fmt.Println("[Debug] Found a match in the db")
+        fmt.Printf("[Debug] Name: %s\n", book.Name)
+        fmt.Printf("[Debug] Author: %s\n", book.Author)
+        fmt.Printf("[Debug] Path: %s\n", book.Path)
     }
 
-    fmt.Println("[Debug] End search a book")
 }
