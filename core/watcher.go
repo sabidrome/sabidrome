5c33535daaa82f9aa55dcf646586bcb085af1b8e
Tomás Villegas <tomas.villegas.m@ug.uchile.cl>
2025-12-14 17:24:35 -0300

    refactor: move watcher to other file

---
+++ core/watcher.go
@@ -0,0 +1,60 @@
+package core
+
+import (
+    "fmt"
+    "strings"
+    "path/filepath"
+    "database/sql"
+
+    "github.com/andreaskoch/go-fswatch"
+)
+
+func FilesystemWatcher(db *sql.DB) {
+
+    recurse := true
+
+    skipDotFilesAndFolders := func(path string) bool {
+        return strings.HasPrefix(filepath.Base(path), ".")
+    }
+
+    checkIntervalInSeconds := 2
+
+    folderWatcher := fswatch.NewFolderWatcher(
+        "/tmq/test",
+        recurse,
+        skipDotFilesAndFolders,
+        checkIntervalInSeconds,
+    )
+
+    folderWatcher.Start()
+
+    for folderWatcher.IsRunning() {
+        select {
+
+            case <-folderWatcher.Modified():
+                // fmt.Println(" -> New or modified items detected")
+
+            case <-folderWatcher.Moved():
+                // fmt.Println(" -> Items have been moved")
+
+            case changes := <-folderWatcher.ChangeDetails():
+                // fmt.Printf("    -> '%s'\n", changes.String())
+                // fmt.Printf("    -> Modified: '%#v'\n", changes.Modified())
+
+                // Remove files are catched here
+                for i:=0; i<len(changes.Moved()); i++ {
+                    book_path := changes.Moved()[i]
+                    fmt.Printf("[Debug] Moved element: %s\n", book_path)
+                    RemoveBook(db, book_path)
+                    ListBookshelf(db)
+                }
+
+                for i:=0; i<len(changes.New()); i++ {
+                    book_path := changes.New()[i]
+                    fmt.Printf("[Debug] New element: %s\n", book_path)
+                    AddBook(db, book_path)
+                    ListBookshelf(db)
+                }
+        }
+    }
+}
